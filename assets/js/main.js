31a32eee9535c43c126170d9a41c8065577acff8
Marco Antonio Gil <gildo_marco@hotmail.com>
2022-11-27 01:15:58 -0300

    Commit inicial

---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+function Calculadora() {
+    //O Input
+    this.display = document.querySelector('.display');
+
+    //o início
+    this.inicia = () => {
+        this.capturaCliques();
+        this.capturaEnter();
+    };
+
+
+
+    //Captura1-Enter
+    this.capturaEnter = () => {
+        this.display.addEventListener('keyup', e => {
+            if (e.keyCode !== 13) return;
+            this.result();
+        });
+    };
+
+    //Captura2-Clique
+    this.capturaCliques = () => {
+        document.addEventListener('click', event => {
+            const el = event.target;
+
+            if (el.classList.contains('btn-num')) this.addNumDisplay(el);
+            if (el.classList.contains('btn-clear')) this.clearNumDisplay();
+            if (el.classList.contains('btn-del')) this.del();
+            if (el.classList.contains('btn-eq')) this.result();
+        });
+    };
+
+
+    //Eventos1
+    this.addNumDisplay = el => {
+        this.display.value += el.innerText;
+        this.display.focus();
+    };
+
+    //Eventos2
+    this.clearNumDisplay = () => this.display.value = "";
+
+    //Eventos3
+    this.del = () => this.display.value = this.display.value.slice(0, -1);
+
+    //Eventos4
+    this.result = () => {
+        try {
+            const conta = eval(this.display.value);
+            if (!conta) {
+                alert('Conta inválida');
+                return
+            }
+            this.display.value = conta;
+
+        } catch (e) {
+            alert('Conta inválida');
+            return;
+        }
+    };
+
+}
+
+//criei um objeto
+const calculadora = new Calculadora();
+calculadora.inicia();
